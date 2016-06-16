5cfd5fbfeecacfe7204d5bed195673d8dc2a704d
jreboqui <jreboqui@ucsd.edu>
2016-06-16 14:34:29 -0700

    Update Framework.py
    Added a game loop so whenever the close button at the upper right is clicked, the window closes.

--- Framework.py
+++ Framework.py
@@ -3,11 +3,23 @@ import pygame
 pygame.init()
 
 gameDisplay = pygame.display.set_mode((800,600))
+pygame.display.set_caption('Slither')
 
-pygame.display.flip() #re-frame, it's like draw
+#pygame.display.flip() #re-frame, it's like draw
 
 pygame.display.update() #only going to update locations you only want to update
 # no parameters will have the same effect as flip
+
+gameExit = False
+
+while not gameExit:
+    for event in pygame.event.get():
+        #exiting the game
+        if event.type == pygame.QUIT:
+            gameExit = True
+    
+
+    
 
 #Exiting pygame
 pygame.quit()
